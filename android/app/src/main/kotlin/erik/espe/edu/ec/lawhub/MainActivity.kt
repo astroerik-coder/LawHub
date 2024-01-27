f6c84671d9d41512abf1bb3952ed4034408bed46
astroerik <dani008molina@gmail.com>
2024-01-26 19:15:16 -0500

    First

---
+++ android/app/src/main/kotlin/erik/espe/edu/ec/lawhub/MainActivity.kt
@@ -0,0 +1,6 @@
+package erik.espe.edu.ec.lawhub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/login/components/users.dart
@@ -0,0 +1,5 @@
+const mockUsers = {
+  'admin@gmail.com': 'admin',
+  'erik@gmail.com': 'erik',
+  'admin123@gmail.com': 'admin123',
+};
