1fa58209aeb2e007994e5db7c674144bf7e6df93
astroerik <dani008molina@gmail.com>
2024-02-22 23:57:23 -0500

    Configure Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lawhub/views/Form2.dart';
 import 'package:lawhub/views/citas/ListaTareasAbogados.dart';
 import 'package:lawhub/views/profile/profile_screen.dart';
 import './views/Inicio.dart';
 import './views/components/bottom_bar.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options:  DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
